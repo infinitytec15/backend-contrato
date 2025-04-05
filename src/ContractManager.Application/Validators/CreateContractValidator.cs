4a2c2d7e2ae164dc689a99aba3952166564f91f5
Gilberto Jr <88731726+infinitytec15@users.noreply.github.com>
2025-04-05 17:40:41 -0300

    Estrutura

---
+++ src/ContractManager.Application/Validators/CreateContractValidator.cs
@@ -0,0 +1,7 @@
+// Deixe esse vazio por enquanto, só crie o arquivo:
+namespace ContractManager.Application.Validators;
+
+public class CreateContractValidator
+{
+    // Em breve usaremos FluentValidation aqui.
+}
