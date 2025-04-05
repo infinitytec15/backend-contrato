9e5cf4d0755c1d9b2f9f2ec74a75f3780043cf4f
Gilberto Jr <88731726+infinitytec15@users.noreply.github.com>
2025-04-05 17:47:34 -0300

    estrutura

---
+++ src/ContractManager.Infrastructure/Repositories/ContractRepository.cs
@@ -0,0 +1,48 @@
+using ContractManager.Domain.Entities;
+using ContractManager.Domain.Interfaces;
+using ContractManager.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContractManager.Infrastructure.Repositories;
+
+public class ContractRepository : IContractRepository
+{
+    private readonly AppDbContext _context;
+
+    public ContractRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Contract contract)
+    {
+        await _context.Contracts.AddAsync(contract);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        var contract = await _context.Contracts.FindAsync(id);
+        if (contract != null)
+        {
+            _context.Contracts.Remove(contract);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<IEnumerable<Contract>> GetAllAsync()
+    {
+        return await _context.Contracts.ToListAsync();
+    }
+
+    public async Task<Contract?> GetByIdAsync(Guid id)
+    {
+        return await _context.Contracts.FindAsync(id);
+    }
+
+    public async Task UpdateAsync(Contract contract)
+    {
+        _context.Contracts.Update(contract);
+        await _context.SaveChangesAsync();
+    }
+}
